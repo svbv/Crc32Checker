c2544ab781bd427bdc32b2279c0544cf5ca052b7
svbv <vsyrtsov@gmail.com>
2016-06-14 12:52:18 +0300

    11
    Signed-off-by: svbv <vsyrtsov@gmail.com>

---
+++ Crc32Checker/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Crc32Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var d = new OpenFileDialog();
+            if (d.ShowDialog() != DialogResult.OK)
+                return;
+
+            var stream = d.OpenFile();
+
+            textBox4.Text = d.FileName;
+            label1.Text = stream.Length.ToString() + " byte";
+
+            var crcExec = new DevExpress.Utils.Zip.Crc32();
+            uint crc = crcExec.Calculate(stream);
+
+            textBox1.Text = crc.ToString("X8");
+            textBox2.Text = crc.ToString();
+            textBox3.Text = ((int)crc).ToString(); ;
+        }
+    }
+}
---
+++ Crc32Checker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Crc32Checker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SHR8MA-")]
+[assembly: AssemblyProduct("Crc32Checker")]
+[assembly: AssemblyCopyright("Copyright © SHR8MA- 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e7cda205-cadd-4f1e-9310-0019dc269044")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
